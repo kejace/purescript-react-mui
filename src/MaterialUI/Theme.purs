f4d4b01e59f4a72df0b18f25352721a94443a5b6
Athan Clark <athan.clark@gmail.com>
2020-05-04 12:36:27 -0700

    more theme fields

--- src/MaterialUI/Theme.purs
+++ src/MaterialUI/Theme.purs
@@ -15,39 +15,47 @@ type ActionTypes = {
   disabledBackground :: String
 }
 
-type Theme = {
-  spacing :: {
-    unit :: Int
-  },
-  breakpoints :: {
-    up :: String -> String
-  },
-  mixins :: {
-    toolbar :: String
-  },
-  palette :: {
-    common :: {
-      black :: String,
-      white :: String
-    },
-    text :: {
-      primary :: String,
-      secondary :: String,
-      disabled :: String,
-      hint :: String
-    }, 
-    primary :: PaletteIntention,
-    secondary :: PaletteIntention,
-    error :: PaletteIntention,
-    background :: {
-      pape :: String,
-      default :: String
-    },
-    divider :: String,
-    action :: ActionTypes
-  },
-  typography :: {
-    fontFamily :: String,
-    pxToRem :: Int -> String
+type Theme =
+  { spacing ::
+    { unit :: Int
+    }
+  , breakpoints ::
+    { up :: String -> String
+    }
+  , mixins ::
+    { toolbar :: String
+    }
+  , palette ::
+    { common ::
+      { black :: String
+      , white :: String
+      }
+    , text ::
+      { primary :: String
+      , secondary :: String
+      , disabled :: String
+      , hint :: String
+      }
+    , primary :: PaletteIntention
+    , secondary :: PaletteIntention
+    , error :: PaletteIntention
+    , background ::
+      { pape :: String
+      , default :: String
+      }
+    , divider :: String
+    , action :: ActionTypes
+    }
+  , typography ::
+    { fontFamily :: String
+    , pxToRem :: Int -> String
+    }
+  , zIndex ::
+    { appBar :: Int
+    , drawer :: Int
+    , mobileStepper :: Int
+    , modal :: Int
+    , snackbar :: Int
+    , tooltip :: Int
+    }
   }
-}
